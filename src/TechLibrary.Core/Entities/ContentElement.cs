4c9c5f3106eadd137a0a2cb70dc36c8c548edb14
stuartm <stuartm@pageuppeople.com>
2016-09-27 12:34:00 +1000

    first commit

---
+++ src/TechLibrary.Core/Entities/ContentElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TechLibrary.Interfaces;
+
+namespace TechLibrary.Domain.Entities
+{
+    public class ContentElement
+    {
+        public ContentElement(string name)
+        {
+            Name = name;
+            ID = Guid.NewGuid();
+        }
+
+        public Guid ID { get; }
+
+        public string Name { get; }
+
+        public int Version { get; set; }
+
+        public IContentElement Content { get; set; }
+
+    }
+}
---
+++ src/TechLibrary.UnitTests/ArticleTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Moq;
+using TechLibrary.Domain.Entities;
+using Xunit;
+
+
+namespace TechLibrary.UnitTests
+{
+    public class ArticleTests : TestBase
+    {
+        [Fact]
+        public void AddSectionToArticle()
+        {
+            var article = GetArticle();
+            article.AddSection(It.IsAny<string>(), It.IsAny<int>());
+            Assert.Equal(1, article.Sections.Count);
+        }
+
+
+        public void AddContentToSection()
+        {
+            var article = GetArticle();
+
+
+        }
+
+
+
+
+
+
+    }
+}
